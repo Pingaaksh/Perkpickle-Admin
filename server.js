609d0ad8f171c6af0eb9e4317c3f382b4d7e3195
dimple <dimple@192.168.1.5>
2024-03-20 12:24:59 +0530

    serever fiel modified

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 app.use(express.static(__dirname + "/dist/<app-name>"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/admin/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/<app-name>/index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
