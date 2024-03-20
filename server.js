ba3f2e7c25386da709bb4d708092722fba63465c
dimple <dimple@192.168.1.5>
2024-03-20 12:56:14 +0530

    chanegs in files

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/<app-name>"));
+// app.use(express.static(__dirname + "/dist/admin"));
+// app.get("/*", function (req, res) {
+//   //res.sendFile(path.join(__dirname + "/dist/<app-name>/index.html"));
+//   const index = path.join(__dirname, "dist/<app-name>/", "index.html");
+//   res.sendFile(index);
+// });
+
+app.use(express.static(path.join(__dirname, "dist/admin")));
 app.get("/*", function (req, res) {
-  //res.sendFile(path.join(__dirname + "/dist/<app-name>/index.html"));
-  const index = path.join(__dirname, "dist/<app-name>/", "index.html");
-  res.sendFile(index);
+  res.sendFile(path.join(__dirname, "dist/admin", "index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
