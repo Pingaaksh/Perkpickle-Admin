23914822204cd512da45a2f2a93e1024ec1764a4
dimple <dimple@dimples-MacBook-Pro.local>
2024-03-29 15:26:23 +0530

    pointed prod url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
-    production: true,
-    // Production
-    apiURL: "https://perk-pickle-7c87ea004712.herokuapp.com",
+    production: false,
+    // staging
+    // apiURL: "https://perk-pickle-7c87ea004712.herokuapp.com",
+    // production
+    apiURL: "https://prod-perkpickle-backend-d2a9ba1cbd6d.herokuapp.com",
 };
