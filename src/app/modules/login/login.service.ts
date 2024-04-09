3d7f233ab02237289c2bcadd41d68950bcc467fd
dimple <dimple@dimples-MacBook-Pro.local>
2024-04-09 12:26:48 +0530

    integrated admin login api

--- src/app/modules/login/login.service.ts
+++ src/app/modules/login/login.service.ts
@@ -2,21 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class LoginService {
-  API_URL = environment.apiURL;
-  userInfo: any;
-  constructor(private http: HttpClient) {}
+    API_URL = environment.apiURL;
+    userInfo: any;
+    constructor(private http: HttpClient) { }
 
-  public authenticateUser(userInfo) {
-    const url = this.API_URL + '/login';
-    return this.http.post<any>(url, userInfo);
-  }
+    public authenticateUser(userInfo) {
+        //const url = this.API_URL + '/login';
+        const url = this.API_URL + 'adminlogin';
+        return this.http.post<any>(url, userInfo);
+    }
 
-  public dashboardCount() {
-    const url = this.API_URL + '/getDashboardCount';
-    return this.http.get<any>(url);
-  }
-  
+    public dashboardCount() {
+        const url = this.API_URL + '/getDashboardCount';
+        return this.http.get<any>(url);
+    }
+
 }
